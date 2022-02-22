f203df40d93849b42cf55668d4eccbb13041e4b8
Woody Wong <20464835@life.hkbu.edu.hk>
2022-02-22 21:46:52 +0800

    debug the heroku 2

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, ], Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 #import configparser
 import logging
